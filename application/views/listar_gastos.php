08a54c169aa5c1954594198572a5b7dc46ab1d29
viniciussn12 <56917996+viniciussn12@users.noreply.github.com>
2021-02-23 01:40:58 -0300

    Criando a view listar_gastos

---
+++ application/views/listar_gastos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Listar Gastos</title>
+</head>
+<body>
+  <h2>Tabela de Gastos</h2>
+  <table>
+    <tr>
+      <th>Data</th>
+      <th>Descrição</th>
+      <th>Valor</th>
+    </tr>
+    <tr>
+      <td>23/02</td>
+      <td>Coxinha</td>
+      <td>2,50</td>
+    </tr>
+</table>
+</body>
+</html>
