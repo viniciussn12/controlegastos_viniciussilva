a4442bffc57eea9c40aaebd5c607d304a107ae69
viniciussn12 <56917996+viniciussn12@users.noreply.github.com>
2021-02-23 10:45:59 -0300

    Criando a view nova_categoria

---
+++ application/views/nova_categoria.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head> 
+  <title>Nova Categoria</title> 
+</head>
+<body>
+  <h1>Nova Categoria</h1>
+  <form action="">
+    <label for="nome">Nome: <input type="text" name="nome"/></label></br>
+    <label for="descricao">Descrição: <input type="text" name="descricao"/></label></br>
+    <button type="submit">Confirmar</button>
+    <button type="reset">Limpar</button>
+  </form>
+</body>
+</html>
