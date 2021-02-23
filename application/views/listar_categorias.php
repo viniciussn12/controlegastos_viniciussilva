a7f33bade0478dc8a96c4dafa3d79b034a2b93ff
viniciussn12 <56917996+viniciussn12@users.noreply.github.com>
2021-02-23 09:42:37 -0300

    Criando a view listar_categorias

---
+++ application/views/listar_categorias.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Lista de Categorias</title>
+</head>
+<body>
+  <ul>
+    <li>Transporte</li>
+    <li>Alimentação</li>
+    <li>Remédios</li>
+    <li>Telefone</li>
+    <li>Internet</li>
+  </ul>
+</body>
+</html>
