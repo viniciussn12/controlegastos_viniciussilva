ed21d1b56c1196bb65053e8920c73c48e3ba77b2
viniciussn12 <56917996+viniciussn12@users.noreply.github.com>
2021-02-23 02:15:18 -0300

    Criando a view novo_gasto

---
+++ application/views/novo_gasto.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head> 
+  <title>Novo Gasto</title> 
+</head>
+<body>
+  <h1>Novo gasto </h1>
+  <form action="">
+    <label for="data">Data: <input type="date" name="data"/></label></br>
+    <label for="descricao">Descrição: <input type="text" name="descricao"/></label></br>
+    <label for="valor">Valor: <input type="number" name="valor"/></label></br>
+    <button type="submit">Confirmar</button>
+    <button type="reset">Limpar</button>
+  </form>
+</body>
+</html>
